317f28be2a2fb6ae5f19918912e602f6bddacf30
tangnewstar <tangscode@163.com>
2024-12-04 10:54:03 +0800

    jump game official answer

--- src/org/tangscode/algorithm/string_and_arrays/jump_game/Solution.java
+++ src/org/tangscode/algorithm/string_and_arrays/jump_game/Solution.java
@@ -48,14 +48,28 @@ public class Solution {
         return false;
     }
 
+    public boolean canJump_Official_Answer(int[] nums) {
+
+        int canJumpMostFar = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i <= canJumpMostFar) {
+                canJumpMostFar = Math.max(canJumpMostFar, i + nums[i]);
+                if (canJumpMostFar >= nums.length - 1) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = {2,3,1,1,4};
-        boolean canJump = solution.canJump(nums);
+        boolean canJump = solution.canJump_Official_Answer(nums);
         System.out.println(canJump);
 
         int[] nums2 = {3,2,1,0,4};
-        boolean canJump2 = solution.canJump(nums2);
+        boolean canJump2 = solution.canJump_Official_Answer(nums2);
         System.out.println(canJump2);
     }
 
@@ -71,6 +85,11 @@ public class Solution {
      *
      * 复杂度分析：时间复杂度O(n^2) 空间复杂度O(n)
      *
-     *
+     * 官方题解
+     * 1. 贪心：不断探索最远可以到达的距离，如果可以到达结束则直接返回，如果探索至结束都不能到达结束点则为False
+     * 复杂度分析：空间O(1) 时间O(n)
+     * 设想一下，对于数组中的任意一个位置 y，我们如何判断它是否可以到达？根据题目的描述，只要存在一个位置 x，它本身可以到达，并且它跳跃的最大长度为 x+nums[x]，这个值大于等于 y，即 x+nums[x]≥y，那么位置 y 也可以到达。
+     * 换句话说，对于每一个可以到达的位置 x，它使得 x+1,x+2,⋯,x+nums[x] 这些连续的位置都可以到达。
+     * 这样以来，我们依次遍历数组中的每一个位置，并实时维护 最远可以到达的位置。对于当前遍历到的位置 x，如果它在 最远可以到达的位置 的范围内，那么我们就可以从起点通过若干次跳跃到达该位置，因此我们可以用 x+nums[x] 更新 最远可以到达的位置。
      */
 }
